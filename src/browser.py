1c8d022e889aa22148ccecd097e68cf88f9244af
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2025-02-24 09:38:22 -0500

    Update

--- src/browser.py
+++ src/browser.py
@@ -109,13 +109,13 @@ class Browser:
 		options.page_load_strategy = "eager"
 
 		seleniumwireOptions: dict[str, Any] = {
-        "verify_ssl": False,
-        "connection_timeout": None,  # Never timeout
-        "read_timeout": None,  # Never timeout
-        "suppress_connection_errors": True,
-        "pool_connections": 100,
-        "pool_maxsize": 100
-    }
+	        "verify_ssl": False,
+	        "connection_timeout": None,  # Never timeout
+	        "read_timeout": None,  # Never timeout
+	        "suppress_connection_errors": True,
+	        "pool_connections": 100,
+	        "pool_maxsize": 100
+	    }
 
 		if self.proxy:
 			# Setup proxy if provided
