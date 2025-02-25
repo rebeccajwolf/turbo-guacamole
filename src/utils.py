27674e27deecd21a4df883b7f1cf93ffb29557f4
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2025-02-24 19:16:36 -0500

    Update

--- src/utils.py
+++ src/utils.py
@@ -35,7 +35,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from urllib3 import Retry
 
 from .constants import REWARDS_URL, SEARCH_URL
-from src.browser import Browser
+from .browser import Browser
 
 class Config(dict):
 	def __init__(self, *args, **kwargs):
