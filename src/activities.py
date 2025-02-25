da8b43b6a262c04982c82168148d581836b87a7a
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2025-02-25 18:58:42 -0500

    Update

--- src/activities.py
+++ src/activities.py
@@ -9,7 +9,7 @@ from selenium.webdriver.remote.webelement import WebElement
 
 from src.browser import Browser
 from src.constants import REWARDS_URL
-from src.utils import CONFIG, sendNotification, getAnswerCode
+from src.utils import CONFIG, sendNotification, getAnswerCode, active_sleep
 
 
 class Activities:
@@ -190,7 +190,7 @@ class Activities:
         except Exception:
             logging.error(f"[ACTIVITY] Error doing {activityTitle}", exc_info=True)
         logging.debug(f"Entering Sleep after Activity")
-        sleep(randint(CONFIG.cooldown.min, CONFIG.cooldown.max))
+        active_sleep(randint(CONFIG.cooldown.min, CONFIG.cooldown.max))
         logging.debug(f"Finished Sleep after Activity")
         self.browser.utils.resetTabs()
 
