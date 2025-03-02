b82c7dda3da9f695290f8640c179864ecc7fc1c3
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2025-03-01 21:03:17 -0500

    Update

--- src/browser.py
+++ src/browser.py
@@ -376,8 +376,8 @@ class Browser:
 		chrome_options = ChromeOptions()
 		chrome_options.add_argument("--headless=new")
 		chrome_options.add_argument("--no-sandbox")
-		# driver = WebDriver(service=ChromeService(getProjectRoot() / "chromedriver"), options=chrome_options)
-		driver = WebDriver(options=chrome_options)
+		driver = WebDriver(service=ChromeService("chromedriver"), options=chrome_options)
+		# driver = WebDriver(options=chrome_options)
 		version = driver.capabilities["browserVersion"]
 
 		driver.close()
