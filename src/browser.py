b8f4ae5f7495ea57821a05632f819186941efb4a
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2025-03-06 14:03:31 -0500

    Update

--- src/browser.py
+++ src/browser.py
@@ -423,3 +423,45 @@ class Browser:
 				driver.quit()
 
 				return version
+
+
+		def getRemainingSearches(
+				self, desktopAndMobile: bool = False
+		) -> RemainingSearches | int:
+				# bingInfo = self.utils.getBingInfo()
+				bingInfo = self.utils.getDashboardData()
+				searchPoints = 1
+				counters = bingInfo["userStatus"]["counters"]
+				pcSearch: dict = counters["pcSearch"][0]
+				pointProgressMax: int = pcSearch["pointProgressMax"]
+
+				searchPoints: int
+				if pointProgressMax in [30, 90, 102]:
+						searchPoints = 3
+				elif pointProgressMax in [50, 150] or pointProgressMax >= 170:
+						searchPoints = 5
+				pcPointsRemaining = pcSearch["pointProgressMax"] - pcSearch["pointProgress"]
+				assert pcPointsRemaining % searchPoints == 0
+				remainingDesktopSearches: int = int(pcPointsRemaining / searchPoints)
+
+				activeLevel = bingInfo["userStatus"]["levelInfo"]["activeLevel"]
+				remainingMobileSearches: int = 0
+				if activeLevel == "Level2":
+						mobileSearch: dict = counters["mobileSearch"][0]
+						mobilePointsRemaining = (
+								mobileSearch["pointProgressMax"] - mobileSearch["pointProgress"]
+						)
+						assert mobilePointsRemaining % searchPoints == 0
+						remainingMobileSearches = int(mobilePointsRemaining / searchPoints)
+				elif activeLevel == "Level1":
+						pass
+				else:
+						raise AssertionError(f"Unknown activeLevel: {activeLevel}")
+
+				if desktopAndMobile:
+						return RemainingSearches(
+								desktop=remainingDesktopSearches, mobile=remainingMobileSearches
+						)
+				if self.mobile:
+						return remainingMobileSearches
+				return remainingDesktopSearches
