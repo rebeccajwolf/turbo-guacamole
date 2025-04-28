815eb17c839f095520b0f22a1ad83b1eaebc77cf
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2025-04-28 18:14:19 -0500

    Update

--- main.py
+++ main.py
@@ -137,7 +137,8 @@ def downloadWebDriver():
 			zip_ref.extractall(temp_dir)
 		
 		# Move the chromedriver to the correct location
-		chromedriver_src = temp_dir / 'chromedriver'
+		# chromedriver_src = temp_dir / 'chromedriver'
+		chromedriver_src = temp_dir / 'chromedriver-linux64' / 'chromedriver'
 		chromedriver_dest = getProjectRoot() / 'chromedriver'
 		
 		# Ensure source file exists
