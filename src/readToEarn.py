4683949518470a37f31cd8789313414d39fb232a
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2025-06-01 01:02:12 -0400

    Update

--- src/readToEarn.py
+++ src/readToEarn.py
@@ -47,11 +47,10 @@ class ReadToEarn:
 		authorization_url, state = mobileApp.authorization_url(
 			authorization_base_url, access_type="offline_access", login_hint=accountName
 		)
-
+		self.webdriver.get(authorization_url)
 		# Get Referer URL from webdriver
 		while True:
-			self.webdriver.get(authorization_url)
-			time.sleep(180)
+			time.sleep(77)
 			logging.info("[READ TO EARN] Waiting for Login")
 			if (
 				self.webdriver.current_url[:48]
@@ -60,6 +59,7 @@ class ReadToEarn:
 				redirect_response = self.webdriver.current_url
 				break
 			time.sleep(7)
+			self.webdriver.refresh()
 
 		logging.info("[READ TO EARN] Logged-in successfully !")
 		# Use returned URL to create a token
